18350935a28c42916a36d14778f918a611d1213c
highjoon <yundosa2@gmail.com>
2022-06-25 20:22:30 +0900

    Recoil, React-router-dom 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.scss'
-import App from './App'
+import { RecoilRoot } from 'recoil'
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+
+import App from './App'
+import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>,
 )
 
