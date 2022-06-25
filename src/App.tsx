03396303a8842e88cfef35476a226c7d14791bd6
highjoon <yundosa2@gmail.com>
2022-06-26 04:06:20 +0900

    Homepage 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import NavBar from '@/composition/NavBar/NavBar'
 import Footer from '@/composition/Footer/Footer'
-import MyPage from './pages/MyPage/MyPage'
+import MyPage from '@/pages/MyPage/MyPage'
+import Home from '@/pages/Home/Home'
 
 function App() {
   return (
     <div id="beer-app-root">
       <div id="beer-app-body">
         <NavBar />
-        <MyPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
         <Footer />
       </div>
     </div>
